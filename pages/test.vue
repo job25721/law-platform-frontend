6acfecec223dd123f06f3dc8eb51177cb960d590
job25721 <pathomporn_pankaew@cmu.ac.th>
2020-07-27 16:22:50 +0700

    init nuxt project

---
+++ pages/test.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <Logo />
+    <h1>Font : TH-SarabunNew</h1>
+    <h1>Law Hackathon H1</h1>
+    <h2>Law Hackathon H2</h2>
+    <p>สวัสดี normal</p>
+    <label>test nuxt-sweetalert</label>
+    <button class="btn btn-danger" @click="test">Alert</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    test() {
+      this.$store.dispatch('alertFn')
+    },
+  },
+}
+</script>
+
+<style scoped>
+p,
+label {
+  font-size: 2rem;
+}
+</style>
