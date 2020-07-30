d6087217b4a0dc9d246c8a1b070207f020283158
Thanatossan <kotokikung@gmail.com>
2020-07-31 01:43:39 +0700

    camera feature

---
+++ pages/camera.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <Nav />
+    <div class="container">
+      <h2>บัตรประชาชน</h2>
+      <div class="camera">
+        <video autoplay class="feed"></video>
+        <button class="">ถ่ายรูป</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  beforeMount() {
+    this.init()
+  },
+  methods: {
+    init() {
+      if (
+        'mediaDevices' in navigator &&
+        'getUserMedia' in navigator.mediaDevices
+      ) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+          const videoPlayer = document.querySelector('video')
+          videoPlayer.srcObject = stream
+          videoPlayer.play()
+        })
+      } else {
+        alert('Cannot get media Devices')
+      }
+    },
+  },
+}
+</script>
+
+<style>
+.camera {
+  width: '100vw';
+  height: '100vh';
+}
+.camera .feed {
+  display: block;
+  margin: auto;
+  width: 50%;
+  max-width: 1280px;
+  box-shadow: 4px 4px 12px 0px rgba(0, 0, 0, 0.25);
+}
+</style>
