10d3ed0f51363b766bc96d3a7df41b29e8703774
Thanatossan <kotokikung@gmail.com>
2020-08-01 22:27:09 +0700

    fix to person and id card

--- pages/camera/index.vue
+++ pages/camera/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <Nav />
-    <div class="container">
-      <h2>บัตรประชาชน</h2>
+    <div class="container p-4">
+      <h2>ถ่ายใบหน้าคู่กับบัตรประชาชน</h2>
       <!-- eslint-disable -->
       <Camera @takePicture="this.takePicture" />
       <!-- eslint-enable -->
-      <Gallery />
+      <!-- <Gallery /> -->
     </div>
   </div>
 </template>
@@ -17,8 +17,10 @@ export default {
     takePicture() {
       const ratio = window.innerHeight < window.innerWidth ? 16 / 9 : 9 / 16
       const picture = document.querySelector('canvas')
-      // picture.width = window.innerWidth < 1280 ? window.innerWidth : 1280
-      // picture.height = window.innerWidth / ratio
+      picture.width =
+        window.innerWidth < 640 ? window.innerWidth : (640 * 3) / 4
+      picture.height =
+        window.innerHeight < 360 ? window.innerHeight : (360 * 3) / 4
       const ctx = picture.getContext('2d')
       ctx.imageSmoothingEnabled = true
       ctx.imageSmoothingQuality = 'high'
@@ -30,6 +32,9 @@ export default {
         picture.height
       )
       console.log(picture.toDataURL('image/jpeg', 1.0))
+
+      // h1.textContent = 'hey'
+      // this.$swal(picture)
     },
   },
 }
